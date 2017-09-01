5f235b162113b25d0a4d39b62c0246c93ddd06ca
Unknown <kolefrazier@mail.weber.edu>
2017-08-31 23:17:03 -0600

    Added project solution and Assignment1 work

---
+++ CS3630ClientSideFrameworks/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CS3630ClientSideFrameworks.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Static HTML page in wwwroot/assignments/ directory.
+        //public IActionResult Assignment1()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
